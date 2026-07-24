929962d91912f14d8860dae06e28a3a92913ca46
d0r0nv0 <deviprasad.rs@walmart.com>
2026-07-24 18:22:06 +0530

    solve Daily Temperatures

---
+++ leetcode/Array/DailyTemperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < n; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIdx = stack.pop();
+                answer[prevIdx] = i - prevIdx;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
